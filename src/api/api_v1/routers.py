ff8af45a15b804953adf43b88651b11eb976e53b
tanjimulislam55 <tanjimulislam118@gmail.com>
2022-05-30 10:02:28 +0600

    rooms, board_types

--- src/api/api_v1/routers.py
+++ src/api/api_v1/routers.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
-from .endpoints import users, hotels
+from .endpoints import users, hotels, boards, rooms
 
 api_router = APIRouter()
 
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(hotels.router, prefix="/hotels", tags=["hotels"])
+api_router.include_router(boards.router, prefix="/board_types", tags=["board_types"])
+api_router.include_router(rooms.router, prefix="/rooms", tags=["rooms"])
