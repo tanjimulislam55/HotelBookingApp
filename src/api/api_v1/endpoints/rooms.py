48fcc6872f618d7d7b6624aee26d544de044e671
tanjimulislam55 <tanjimulislam118@gmail.com>
2022-05-31 16:10:20 +0600

    feat: add room

--- src/api/api_v1/endpoints/rooms.py
+++ src/api/api_v1/endpoints/rooms.py
@@ -1,25 +1,30 @@
-from typing import List
-from fastapi import APIRouter, Depends, status, UploadFile, File, Request
+from fastapi import APIRouter, Depends, status
 from fastapi.exceptions import HTTPException
-import aiofiles
-import os
 
-from schemas import RoomCreate, RoomOut, RoomUpdate, ImageCreate, ImageBase
+from schemas import RoomCreate, RoomOut, AmenityCreate
 from models import User
 from api.dependencies import get_current_active_superuser
-from crud.rooms import room
+from crud.rooms import room, amenity
+from settings import settings
 
 router = APIRouter()
 
 
-# @router.post("/awesome")
-# async def post_form(file_in: UploadFile = File(...)):
-#     async with aiofiles.open(file_in.filename, "wb") as f:
-#         filename = os.path.join("static/", file_in.filename)
-#         if not filename:
-#             while content := await file_in.read(1024):  # async read chunk
-#                 await f.write(content)
-#             return {
-#                 "Uploaded File": file_in.filename,
-#             }
-#     return "already exists"
+@router.post("/")
+async def create_new_room(room_in: RoomCreate):
+    new_generated_room_id = await room.create(room_in.copy(exclude={"amenity"}))
+    room_dict = room_in.copy(exclude={"amenity"}).dict()
+    amenity_dict = room_in.amenity.dict()
+    amenity_dict.update({"room_id": new_generated_room_id})
+    try:
+        await amenity.create(AmenityCreate(**amenity_dict))
+        return {
+            "id": new_generated_room_id,
+            **room_dict,
+            **amenity_dict,
+        }
+    except NotImplementedError:
+        await room.remove(new_generated_room_id)
+        raise HTTPException(
+            status_code=status.HTTP_501_NOT_IMPLEMENTED, detail="Could not create."
+        )
