29d33b3584ce0db7bc77ae6b308f6962264ecc2b
tanjimulislam55 <tanjimulislam118@gmail.com>
2022-05-31 16:12:07 +0600

    incomplete

---
+++ src/schemas/images.py
@@ -0,0 +1,21 @@
+from fastapi import UploadFile, File
+from typing import Optional
+from pydantic import BaseModel
+
+
+class ImageBase(BaseModel):
+    title: Optional[str]
+    description: Optional[str]
+
+
+class ImageCreate(ImageBase):
+    source_url: Optional[str]
+    file_name: Optional[str]
+    room_id: int
+
+
+class ImageOut(ImageCreate):
+    id: int
+
+    class Config:
+        orm_mode = True
